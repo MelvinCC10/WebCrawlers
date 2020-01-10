65106feb7b0125f272169d42c4ff6754139dd8dc
Reven <Melvin.cordon21@gmail.com>
2020-01-09 23:39:40 -0600

    created a "stock" class

--- Stock Headlines/get_headlines.py
+++ Stock Headlines/get_headlines.py
@@ -2,14 +2,29 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
+class stock():
+
+    def __init__(self,ticker):
+        self.url = 'https://www.marketwatch.com/investing/stock/' + ticker
+
+    def price(self):
+        page = urlopen(self.url)
+        soup = BeautifulSoup(page,'html.parser')
+        find_price = soup.find('bg-quote', attrs={'class':'value'})
+        price = find_price.text.strip()
+
+        return price
+
+    def headlines(self):
+        page = urlopen(self.url)
+        soup = BeautifulSoup(page,'html.parser')
+        find_price = soup.find('h3', attrs={'class':'article_headline'})
+        print(find_price)
+        price = find_price.text.strip()
 
 
-web_page = 'https://www.marketwatch.com/investing/index/djia?mod=home-page'
-web_page_html = urlopen(web_page)
-page = web_page_html.read()
-
-soup = BeautifulSoup(page,'html.parser')
-Stock_price = soup.find(‘div’, attrs={‘class’:’intraday__data’})
 
 
-#price_box = soup.find(‘sup’, attrs={‘class’:’character’})
+
+tesla_stock = stock('tsla')
+print(tesla_stock.headlines())
